0d84d2273f919540451a25552d90a4415e5b23e9
karyRmartinez <karymartinez@pursuit.org>
2021-02-15 14:24:46 -0500

    added image set

--- Hops-Hackaton/Hops-Hackaton/HomeViewController.swift
+++ Hops-Hackaton/Hops-Hackaton/HomeViewController.swift
@@ -13,18 +13,45 @@ class HomeViewController: UIViewController {
     lazy var titleLabel: UILabel = {
          let label = UILabel()
          label.backgroundColor = .white
-         label.textColor = .systemBlue
+        label.textColor = .systemGreen
          label.textAlignment = .center
          label.text = "HOPS"
          label.font = UIFont(name: "savoyeLetPlain", size: 50)
          label.translatesAutoresizingMaskIntoConstraints = false
          return label
      }()
+    
+    lazy var imageViewSet : UIImageView = {
+    let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "ImageEars")
+           return image
+       }()
+       
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        addSubviews()
+        settingUpConstraints()
 
+    }
+    private func addSubviews() {
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(imageViewSet)
+    }
+    private func settingUpConstraints() {
+        NSLayoutConstraint.activate([
+    
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120),
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        
+        imageViewSet.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 40),
+        imageViewSet.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        imageViewSet.widthAnchor.constraint(equalToConstant: 200),
+        imageViewSet.heightAnchor.constraint(equalToConstant: 200),
+        ])
+    }
 
 }
 
